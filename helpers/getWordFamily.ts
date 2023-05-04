4c80947e53c8fad9f73579b749f7edcae7eb3a2b
Leandro Arturi <lea.arturi@gmail.com>
2023-05-03 21:04:12 -0300

    Validate error on fetching

--- helpers/getWordFamily.ts
+++ helpers/getWordFamily.ts
@@ -13,27 +13,33 @@ export default async function getWordFamily() {
    // The random word must have verb, noun, adjective and adverb
    while (existsNullArrays) {
       randomWord = getRandomWord();
-      const response = await fetch(
-         `http://apirest.com.ar:8900/random_word_family/${randomWord}`
-      );
 
-      // const response = await fetch(
-      //    `http://127.0.0.1:8000/random_word_family/${randomWord}`
-      // );
+      try {
+         const response = await fetch(
+            `http://apirest.com.ar:8900/random_word_family/${randomWord}`
+         );
 
-      wordForms = await response.json();
+         // const response = await fetch(
+         //    `http://127.0.0.1:8000/random_word_family/${randomWord}`
+         // );
 
-      const countVerbs = wordForms.word_forms.v.length;
-      const countNouns = wordForms.word_forms.n.length;
-      const countAdjective = wordForms.word_forms.a.length;
-      const countAdverb = wordForms.word_forms.r.length;
+         wordForms = await response.json();
 
-      // Check if any array have not elements
-      existsNullArrays =
-         countVerbs === 0 ||
-         countNouns === 0 ||
-         countAdjective === 0 ||
-         countAdverb === 0;
+         const countVerbs = wordForms.word_forms.v.length;
+         const countNouns = wordForms.word_forms.n.length;
+         const countAdjective = wordForms.word_forms.a.length;
+         const countAdverb = wordForms.word_forms.r.length;
+
+         // Check if any array have not elements
+         existsNullArrays =
+            countVerbs === 0 ||
+            countNouns === 0 ||
+            countAdjective === 0 ||
+            countAdverb === 0;
+      } catch (error) {
+         randomWord = '...';
+         existsNullArrays = false;
+      }
    }
 
    return { wordForms, randomWord };
