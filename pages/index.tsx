429b0104dd7a2d08d2f2ec6dd6e5fdab411fc05e
Leandro Arturi <lea.arturi@gmail.com>
2023-05-04 22:17:11 -0300

    Title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head';
 import Form from '../components/Form';
 
 const IndexPage = () => {
-   return <Form />;
+   return (
+      <>
+         <Head>
+            <title>Word Forms Practice!</title>
+         </Head>
+         <Form />
+      </>
+   );
 };
 
 export async function getServerSideProps() {
