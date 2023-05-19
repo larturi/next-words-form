4d96cdfa6986a8f4ab67a8a84263b730a10186f1
Leandro Arturi <lea.arturi@gmail.com>
2023-05-19 07:06:12 -0300

    Cors

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,3 +1,3 @@
 export const URL_SERVICE_WORD_FAMILY = 'https://apirest.com.ar:8900'
-export const URL_SERVICE_TRANSLATE_WORD = 'http://cloudapp.com.ar/demo/php-translate-api/www/'
+export const URL_SERVICE_TRANSLATE_WORD = 'https://apirest.com.ar/php-translate-api/www/'
 export const WORD_FAMILY_NON_EMPTY_ARRAYS = 2
