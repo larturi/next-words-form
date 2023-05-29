af8f8e27ef94188e8945eb2f52d12a9c17a7c13b
Leandro Arturi <lea.arturi@gmail.com>
2023-05-29 07:52:58 -0300

    Env

--- README.md
+++ README.md
@@ -15,4 +15,20 @@ npm run dev
     docker build -t larturi/wordsform-next-app:latest .
     docker push larturi/wordsform-next-app:latest
     docker run -p 3000:3000 --name wordsform-next-app larturi/wordsform-next-app
+
+    minikube addons enable ingress
+    minikube ip
+    # Add to etc/hots
+    # <minikube-ip>   wordforms.larturi.local
+
+    cd ingress/
+    kubectl apply -f .
+
+    kubectl apply -f 01-fastapi-deployment.yaml
+    kubectl apply -f 02-php-deployment.yaml
+    kubectl apply -f 03-next-deployment.yaml
+    kubectl apply -f 04-fastapi-service.yaml
+    kubectl apply -f 05-php-service.yaml
+    kubectl apply -f 06-next-service.yaml
+    kubectl apply -f 07-ingress.yaml
 ```
--- constants/constants.ts
+++ constants/constants.ts
@@ -1,9 +1,3 @@
-// Local
-// export const URL_SERVICE_WORD_FAMILY = 'https://apirest.com.ar:8900'
-// export const URL_SERVICE_TRANSLATE_WORD = 'https://apirest.com.ar/php-translate-api/www/'
-// export const WORD_FAMILY_NON_EMPTY_ARRAYS = 2
-
-// Production
-export const URL_SERVICE_WORD_FAMILY = 'http://localhost:8900'
-export const URL_SERVICE_TRANSLATE_WORD = 'http://localhost:8080'
-export const WORD_FAMILY_NON_EMPTY_ARRAYS = 2
+export const URL_SERVICE_WORD_FAMILY = process.env.NEXT_PUBLIC_URL_SERVICE_WORD_FAMILY || 'http://localhost:8900'
+export const URL_SERVICE_TRANSLATE_WORD = process.env.NEXT_PUBLIC_URL_SERVICE_TRANSLATE_WORD || 'http://localhost:8080'
+export const WORD_FAMILY_NON_EMPTY_ARRAYS = process.env.NEXT_PUBLIC_WORD_FAMILY_NON_EMPTY_ARRAYS || 2
