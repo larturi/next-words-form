d66169ecd358cf0b2f035c34de1ac67b8d231963
Leandro Arturi <lea.arturi@gmail.com>
2023-05-28 23:23:08 -0300

    Dockerfile

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ npm install
 
 npm run dev
 ```
+
+## Production
+
+```bash
+    docker build -t larturi/wordsform-next-app:latest .
+    docker push larturi/wordsform-next-app:latest
+    docker run -p 3000:3000 --name wordsform-next-app larturi/wordsform-next-app
+```
--- constants/constants.ts
+++ constants/constants.ts
@@ -1,3 +1,9 @@
-export const URL_SERVICE_WORD_FAMILY = 'https://apirest.com.ar:8900'
-export const URL_SERVICE_TRANSLATE_WORD = 'https://apirest.com.ar/php-translate-api/www/'
+// Local
+// export const URL_SERVICE_WORD_FAMILY = 'https://apirest.com.ar:8900'
+// export const URL_SERVICE_TRANSLATE_WORD = 'https://apirest.com.ar/php-translate-api/www/'
+// export const WORD_FAMILY_NON_EMPTY_ARRAYS = 2
+
+// Production
+export const URL_SERVICE_WORD_FAMILY = 'http://localhost:8900'
+export const URL_SERVICE_TRANSLATE_WORD = 'http://localhost:8080'
 export const WORD_FAMILY_NON_EMPTY_ARRAYS = 2
